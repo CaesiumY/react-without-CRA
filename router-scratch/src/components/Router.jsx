3febdb508794aba27a44734c08341daa3599500a
Changsik Yoon <dbs2636@gmail.com>
2021-10-22 20:50:39 +0900

    feat: Add vanilla hash router

--- router-scratch/src/components/Router.jsx
+++ router-scratch/src/components/Router.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Router = ({ mapping }) => {
-  return <p>router</p>;
+  const [hash, setHash] = useState(window.location.hash);
+
+  const setCurrentHash = () => setHash(window.location.hash);
+
+  useEffect(() => {
+    window.addEventListener("hashchange", setCurrentHash);
+
+    return () => {
+      window.removeEventListener("hashchange", setCurrentHash);
+    };
+  }, []);
+
+  if (mapping[hash]) return mapping[hash];
+  else return mapping["*"];
 };
 
 export default Router;
--- router-scratch/src/index.js
+++ router-scratch/src/index.js
@@ -4,4 +4,26 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import Router from "./components/Router";
 
-ReactDOM.render(<Router />, document.getElementById("app"));
+const mapping = {
+  "#profile": (
+    <div>
+      Profile (<a href="#">home</a>)
+    </div>
+  ),
+  "#account": (
+    <div>
+      Account (<a href="#">home</a>)
+    </div>
+  ),
+  "*": (
+    <div>
+      Dashboard <br />
+      <a href="#profile">Profile</a>
+      <br />
+      <a href="#account">Account</a>
+      <br />
+    </div>
+  ),
+};
+
+ReactDOM.render(<Router mapping={mapping} />, document.getElementById("app"));
