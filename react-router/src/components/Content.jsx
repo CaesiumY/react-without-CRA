02b5d2f49148eceb70d4635b5c49304353adebc4
Changsik Yoon <dbs2636@gmail.com>
2021-10-23 02:06:24 +0900

    feat: Add reatc-router samples

--- react-router/src/components/Content.jsx
+++ react-router/src/components/Content.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 
-const Content = (props) => {
+const Content = ({ children }) => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-nav">
-        <li>
-          <NavLink to="/about" activeClassName="active">
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/posts" activeClassName="active">
-            Blog
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <h1>Node University</h1>
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li>
+            <NavLink to="/about" activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts" activeClassName="active">
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active">
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
   );
 };
 
--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, withRouter } from "react-router-dom";
+import "./css/index.css";
 import Content from "./components/Content";
 import About from "./components/About";
+import Posts from "./components/Posts";
+import Contact from "./components/Contact";
 
-import "./css/index.css";
+const WithRouterContact = withRouter(Contact);
+const WithRouterPosts = withRouter(Posts);
+
+const posts = {
+  title: "Post",
+  content: "Post Content",
+};
 
 ReactDOM.render(
   <HashRouter>
@@ -12,6 +21,15 @@ ReactDOM.render(
       <Content />
       <Route path="/about">
         <About />
+      </Route>
+      <Route exact path="/posts">
+        <Posts posts={posts} />
+      </Route>
+      <Route path="/posts/:id">
+        <WithRouterPosts posts={posts} />
+      </Route>
+      <Route path="/contact">
+        <WithRouterContact />
       </Route>
     </Route>
   </HashRouter>,
